fedf469c35cdf5c44711cd5b72dcb553cc8afc9d
Santosh Kumar <dssant@gmail.com>
2020-03-10 00:23:47 +0530

    Update NB_Classification_Model.py

--- Navie Bayes/NB_Classification_Model.py
+++ Navie Bayes/NB_Classification_Model.py
@@ -1,4 +1,6 @@
+# Import pandas library for data manipulation 
 import pandas as pd
+# Import numpy library for calculation 
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.model_selection as train_test_split
