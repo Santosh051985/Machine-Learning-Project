2152dfcdbc6063ac81a7f614ffb7e23e2a7885b5
Santosh Kumar <dssant@gmail.com>
2020-03-26 11:04:50 +0530

    Update MLPC.py

--- MLPC.py
+++ MLPC.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 wbcd = pd.read_csv("C:\\Users\\HP\\Desktop\\wbcd.csv")
 # Shows features of dataset
 wbcd.columns
-# Dropping the uncessary column which has not involve in calculation
+# Dropping the  column which has not involve in calculation
 wbcd.drop(["id"],axis=1,inplace=True) 
 # Handle No missing values 
 wbcd.isnull().sum() 
